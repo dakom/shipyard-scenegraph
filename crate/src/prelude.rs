368f217a6bf2790086439a49be08de00c7aa71d8
David Komer <david.komer@gmail.com>
2022-02-23 23:19:03 +0200

    nalgebra_glm

--- crate/src/prelude.rs
+++ crate/src/prelude.rs
@@ -2,6 +2,10 @@
 /// Only if nalgebra_math feature is enabled
 pub use crate::math::nalgebra::*;
 
+#[cfg(feature = "nalgebra_glm_math")]
+/// Only if nalgebra_math feature is enabled
+pub use crate::math::nalgebra_glm::*;
+
 #[cfg(feature = "native_math")]
 /// Only if native_math feature is enabled
 pub use crate::math::native::*;
